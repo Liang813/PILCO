3cc0a5fc2f2f739232e82cf307f7f51fe3c8d554
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-05-17 15:50:51 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+from pilco.rewards import ExponentialReward
+import tensorflow as tf
+import numpy as np
+with tf.Session(graph=tf.Graph()) as sess:
+    R = ExponentialReward(state_dim=2, t=np.array([0.,0.]),W=np.ones((2,2)))
+    muRs = lambda th:R.compute_reward(np.array([np.sin(th),np.cos(th)]), s=np.eye(2))
+    left = [i/36*2*np.pi for i in range(36)]
+    height = np.array([muRs(th)[0].eval()[0][0] for th in left])
+plt.xlabel('rad')
+plt.ylabel('mean of reward')
+plt.plot(left, height,label='ones')
+plt.legend()
